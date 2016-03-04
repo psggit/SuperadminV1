a94d302f5a7aa041c0ec2214fd0cc133101bcde1
Shrilakshmi <shrilakshmi210@gmail.com>
2016-03-04 13:14:00 +0530

    KYC files

---
+++ src/components/Consumer/Kycfunctions.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { Link } from 'react-router';
+
+const Kycfunctions = ({dispatch}) => { // eslint-disable-line no-unused-vars
+  const styles = require('./kycfunctions.scss');
+  // Force re-rendering of children using key: http://stackoverflow.com/a/26242837
+  return (
+      <div className={styles.container + ' container-fluid'}>
+      <div className = {styles.leftsquarebox}>
+    <div className={styles.squarecontent}>
+    <div><span><Link to={'/consumer/kycfunctions/kycview'}>Verify KYC</Link></span></div>
+    </div>
+  </div>
+  <div className = {styles.rightsquarebox}>
+    <div className={styles.squarecontent}>
+    <div><span><Link to={'/consumer/kycfunctions/uploadkyc'}>Upload KYC</Link></span></div>
+    </div>
+</div>
+</div>
+);
+};
+
+const mapStateToProps = (state) => {
+  return {...state.profile};
+};
+
+export default connect(mapStateToProps)(Kycfunctions);
