f3482e9277b766e41500822f7cba4a42cfac013c
Karthik Venkateswaran <vasanthivenkateswaran@gmail.com>
2016-11-28 18:58:25 +0530

    Updated Page Container

--- src/components/PageContainer/PageContainer.js
+++ src/components/PageContainer/PageContainer.js
@@ -143,11 +143,13 @@ const PageContainer = ({location, children, dispatch}) => { // eslint-disable-li
             <li><input type="checkbox" id="cb5"/><label htmlFor="cb5">RETAILER MANAGEMENT</label>
               <ul>
               <li className={styles.sidebar_list}><label>
-                  <Link to={'/hadmin/retailer_management/profile_and_kyc'} className={ lastPathname === 'profiles' ? styles.active : '' }> Profile & KYC </Link>
+                  <Link to={'/hadmin/retailer_management/profile_and_kyc'} > Profile & KYC </Link>
               </label></li>
+              {/*
               <li className={styles.sidebar_list}><label>
                   <Link to={'/hadmin/consumer/profiles'} className={ lastPathname === 'profiles' ? styles.active : '' }> Transactions </Link>
               </label></li>
+              */}
               </ul>
             </li>
           </ul>
@@ -159,12 +161,12 @@ const PageContainer = ({location, children, dispatch}) => { // eslint-disable-li
               <ul>
                 <li className={styles.sidebar_list}>
                   <label>
-                    <Link to={'/hadmin/bar_management/bar_profile_and_kyc'} className={ lastPathname === 'profiles' ? styles.active : '' }> Profile & KYC </Link>
+                    <Link to={'/hadmin/bar_management/bar_profile_and_kyc'} > Profile & KYC </Link>
                   </label>
                 </li>
                 <li className={styles.sidebar_list}>
                   <label>
-                    <Link to={'/hadmin/bar_management/bar_sku_landing'} className={ lastPathname === 'profiles' ? styles.active : '' }> BAR SKU MANAGEMENT </Link>
+                    <Link to={'/hadmin/bar_management/bar_sku_landing'} > BAR SKU MANAGEMENT </Link>
                   </label>
                 </li>
               </ul>
