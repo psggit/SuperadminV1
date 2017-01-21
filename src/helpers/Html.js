f94ccb49071e429b505debbc637abd60244c421f
Verghese Koshy <koshy@34hasura.io>
2017-01-21 19:50:27 +0530

    Added a hacked-view to see the promos associated with the campaign

--- src/helpers/Html.js
+++ src/helpers/Html.js
@@ -34,7 +34,7 @@ export default class Html extends Component {
             {/*head.script.toComponent()*/}
             {/*
             <script src="https://cdn.logrocket.com/LogRocket.min.js"></script>
-            
+
             <script dangerouslySetInnerHTML={{ __html: 'window.LogRocket.init("mnm-studios/mambajamba", { shouldShowReportingButton: true, })' }} charSet="UTF-8" />
             */}
 
@@ -52,7 +52,7 @@ export default class Html extends Component {
             {/* ideally one could also include here the style for the current page (Home.scss, About.scss, etc) */}
             {/* Object.keys(assets.styles).length === 0 ? <style dangerouslySetInnerHTML={{__html: require('../theme/bootstrap.config.js') + require('../containers/App/App.scss')._style}}/> : null */}
 
-            {/*FIXME: Use this for serializing SSR data
+            {/* FIXME: Use this for serializing SSR data
             <script dangerouslySetInnerHTML={{__html: `window.__data=${serialize(store.getState())};`}} charSet="UTF-8"/>*/}
             <link href='https://fonts.googleapis.com/css?family=Roboto:400,500' rel='stylesheet' type='text/css' />
             <script dangerouslySetInnerHTML={{__html: `
@@ -60,6 +60,10 @@ export default class Html extends Component {
                   baseDomain: '${process.env.BASE_DOMAIN}'
                 };
               `}} />
+            <script src="https://code.jquery.com/jquery-1.12.4.min.js" integrity="sha256-ZosEbRLbNQzLpnKIkEdrPv7lOy9C27hHQ+Xp8a4MxAQ="
+            crossOrigin="anonymous"></script>
+            <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa"
+              crossOrigin="anonymous"></script>
         </head>
         <body>
             <div className="content_wrapper" id="content" dangerouslySetInnerHTML={{__html: content}}/>
