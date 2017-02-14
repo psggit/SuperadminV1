3b71ff5f92999d377ed0bd71a75f0858ea0c50ef
Harshit Lingamneni <harshit@hasura.io>
2017-02-15 02:42:23 +0530

    Promo insert update on cash_back_offer_sku to add quantity_left

--- src/components/BrandsOffersAndPromos/PromosCashbackRedeem/fetchQueries.js
+++ src/components/BrandsOffersAndPromos/PromosCashbackRedeem/fetchQueries.js
@@ -175,7 +175,8 @@ const insertCampaignAndPromos = {
             // better to make bulk inserts in one query TODO: fix this later
             price: promo.type === 'amount' ? (promo.price * promo.quantity) : ((((promo.price / 100)) * promo.pricing.price) * promo.quantity),
             offer_id: offers[index].returning[0].id,
-            quantity: (promo.quantity ? parseInt(promo.quantity, 10) : 0)
+            quantity: (promo.quantity ? parseInt(promo.quantity, 10) : 0),
+            quantity_left: (promo.quantity ? parseInt(promo.quantity, 10) : 0)
           }]
         }
       };
