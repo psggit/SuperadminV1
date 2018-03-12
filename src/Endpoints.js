2833f8ff847014cc3a9dcae42e371064e6227cce
syed <syed@hipbar.com>
2018-03-12 17:34:08 +0530

    Added Brand Resolutions

--- src/Endpoints.js
+++ src/Endpoints.js
@@ -13,7 +13,7 @@ let scheme;
 let baseHost = 'TEST';
 
 if (window.location.href.split(':')[1] === '//localhost') {
-  const appName = 'hearsay81';
+  const appName = 'amebae21';
   // const appName = 'hipbar-stg';
   dataUrl = 'https://data.' + appName + '.hasura-app.io';
   authUrl = 'https://auth.' + appName + '.hasura-app.io';
